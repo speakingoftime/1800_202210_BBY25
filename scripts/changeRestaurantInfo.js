ea5ca4fbffdeaf3c2a91f1bf9d2ce2735e97e891
Tracy Ly <tly35@my.bcit.ca>
2022-03-04 15:38:11 -0800

    load data from Restaurants collection to change placeholder restaurant name, ratings

--- scripts/changeRestaurantInfo.js
+++ scripts/changeRestaurantInfo.js
@@ -1,111 +1,58 @@
-let dummyData = {
-  "restaurants": [
-    {
-      "name": "John's Pasta",
-      "food": [
-        100,
-        18
-      ],
-      "value": [
-        20,
-        89
-      ],
-      "service": [
-        80,
-        22
-      ],
-      "language": [
-        20,
-        65
-      ]
-    },
-    {
-      "name": "Sally's Sushi",
-      "food": [
-        88,
-        56
-      ],
-      "value": [
-        77,
-        22
-      ],
-      "service": [
-        80,
-        220
-      ],
-      "language": [
-        221,
-        65
-      ]
-    },
-    {
-      "name": "Frank's Noodles",
-      "food": [
-        111,
-        2
-      ],
-      "value": [
-        300,
-        1
-      ],
-      "service": [
-        2,
-        111
-      ],
-      "language": [
-        100,
-        1
-      ]
-    },
-    {
-      "name": "Pam's Steakhouse",
-      "food": [
-        120,
-        1
-      ],
-      "value": [
-        2,
-        99
-      ],
-      "service": [
-        800,
-        2
-      ],
-      "language": [
-        2,
-        650
-      ]
-    },
-    {
-      "name": "Bianca's Pizza",
-      "food": [
-        1020,
-        8
-      ],
-      "value": [
-        200,
-        89
-      ],
-      "service": [
-        802,
-        2
-      ],
-      "language": [
-        200,
-        5
-      ]
-    }
-  ]
-}
+// Get the url value after ?
+const queryString = window.location.search;
+const queryValue = queryString.slice(1, queryString.length);
+// Format the restaurant name taken from the URL
+let queryValueFmt = queryValue.replaceAll("%27", "'").replaceAll("%20", " ");
+let restPageName = queryValueFmt;
 
+let restPageRatings = [];
 
+db.collection("restaurants").get()
+  .then(snap => {
+    snap.forEach(doc => {
+        // Load the restaurant's name + ratings
+        if (restPageName === doc.data().name) {
+          restPageRatings.push({
+            "food": doc.data().food,
+            "service": doc.data().service,
+            "value": doc.data().value,
+            "language": doc.data().language
+          });
+        }
+    })
+  }).then(() => {
+    // Changes restaurant name
+    document.getElementById("rest-name-placeholder").innerText = restPageName;
 
-// card.onclick = function() {
-  
-// }
+    // Creates a div to hold all the ratings
+    let restRatingsDiv = document.createElement("div");
+    let restRatingsContent = document.getElementById("rest-ratings-content");
+    restRatingsContent.appendChild(restRatingsDiv);
 
-$.load("./restaurant.html", function() {
-  let restName = document.getElementById("rest-name-placeholder");
-  restName = dummyData.name;
-});
+    restPageRatings.forEach(doc => {
+      // food rating
+      const i = parseInt((doc.food[0] / (doc.food[0] + doc.food[1]) * 100));
+      let foodRatingDiv = document.createElement("div");
+      restRatingsDiv.appendChild(foodRatingDiv);
+      foodRatingDiv.innerHTML = "Food Quality: " + i + "% &#128077;";
 
+      // value rating
+      const j = parseInt((doc.value[0] / (doc.value[0] + doc.value[1]) * 100));
+      let valRatingDiv = document.createElement("div");
+      restRatingsDiv.appendChild(valRatingDiv);
+      valRatingDiv.innerHTML = "Value: " + j + "% &#128077;";
+
+      // service rating
+      const k = parseInt((doc.service[0] / (doc.service[0] + doc.service[1]) * 100));
+      let servRatingDiv = document.createElement("div");
+      restRatingsDiv.appendChild(servRatingDiv);
+      servRatingDiv.innerHTML = "Service: " + k + "% &#128077;";
+
+      // language rating
+      const l = parseInt((doc.language[0] / (doc.language[0] + doc.language[1]) * 100));
+      let langRatingDiv = document.createElement("div");
+      restRatingsDiv.appendChild(langRatingDiv);
+      langRatingDiv.innerHTML = "Language Independency: " + l + "% &#128077;";
+    });
+
+  })
