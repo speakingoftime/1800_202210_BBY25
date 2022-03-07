a3fbd0446aca3b6eaa54c6dea92f0b469d629368
Tracy Ly <tly35@my.bcit.ca>
2022-03-07 00:15:54 -0800

    added authentication script

---
+++ scripts/authentication.js
@@ -0,0 +1,56 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+
+            //Get the user object info
+            var user = authResult.user;
+            if (authResult.additionalUserInfo.isNewUser) {
+                //Create a collection with name "users"
+                db.collection("users")
+                //Define a document for a user with userID (UID) as a document ID
+                .doc(user.uid).set({
+                    name: user.displayName,
+                    email: user.email
+                }).then(function(){
+                    console.log("New user added to Firestore");
+                    window.localStorage.assign("main.html");
+                })
+                .catch(function(error){
+                    console.log(error);
+                });
+            } else {
+                return true;
+            }
+            return false;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'main.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
