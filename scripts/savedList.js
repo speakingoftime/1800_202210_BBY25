f2d83570f3463f2205b57f373f1c66f612f9e328
Tracy Ly <tly35@my.bcit.ca>
2022-03-12 04:05:53 -0800

    fixed save btn adding duplicate entries, revised btn methods, added modal prompt to login

--- scripts/savedList.js
+++ scripts/savedList.js
@@ -1,7 +1,20 @@
 let saveBtn = document.getElementById("btn-check-outlined");
 let restName = localStorage.getItem("restaurant name");
 console.log(restName);
+let count = 0;
 
+function toggleSaveBtn() {
+  if (count === 0) {
+    console.log("Saved");
+    saveBtn.classList.add("active");
+    saveBtn.querySelector(".saveIcon").innerText = "favorite";
+  }  
+  else {
+    console.log("Removed");
+    saveBtn.classList.remove("active");
+    saveBtn.querySelector(".saveIcon").innerText = "favorite_border";
+  };
+}
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -17,44 +30,33 @@ firebase.auth().onAuthStateChanged((user) => {
             return true;
           }
         });
-        console.log("Undefined? " + match); 
         // If the restaurant is in the user's list, 
         // change the button's state to the appropriate one
-        let count = 0;
-        if (match == undefined) {
-          saveBtn.removeAttribute("checked");
-          count = 0;
-        } else {
+        if (match !== undefined) {
+          saveBtn.classList.add("active");
+          saveBtn.querySelector(".saveIcon").innerText = "favorite";
           count = 1;
         }
-
         // Add restaurant to user's saved list on click of button
         saveBtn.addEventListener("click", function() {
+          toggleSaveBtn();
+          // Filter restaurant name stored in localStorage from the user's saved restaurants
+          let filterData = currentData.filter(function(result) {
+            return result != restName; // Returns a new array
+          });
           if (count == 0) {
-            console.log("Saved");
-            count++;
-            // Get restaurant name from local storage
-            let tempData = [restName];
-            // Concatenate restaurant name to top of current list
-            let newData = tempData.concat(currentData);
-            // Rewrite list
+            let newData = [restName].concat(filterData);
             savedList.update({
               restaurants: newData
             });
+            count++;
           } else if (count == 1) {
-            console.log("Removed");
-            count--;
-            // Filter restaurant name stored in localStorage from the user's saved restaurants
-            let tempData = currentData.filter(function(result) {
-              return result != restName; // Returns a new array
-            })
-            // Rewrite list
             savedList.update({
-              restaurants: tempData
+              restaurants: filterData
             });
-          }
-        })
-        
+            count--;
+          };
+        });
       } else {
         // doc.data() will be undefined
         console.log("No such document!");
@@ -65,6 +67,16 @@ firebase.auth().onAuthStateChanged((user) => {
   } else {
     // User is signed out
     console.log("Not logged in");
-    saveBtn.removeAttribute("checked");
-  }
+    let count = 0;
+    saveBtn.addEventListener("click", function() {
+      // Load modal prompting user to log in
+      let loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+      loginModal.toggle(loginModal);
+      // Ensure the button does not look active while not logged in
+      if (count == 0) {
+        saveBtn.classList.remove("active");
+        saveBtn.querySelector(".saveIcon").innerText = "favorite_border";
+        count++;
+      };
+  })};
 });
